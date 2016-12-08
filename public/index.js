22c7e038bdad6bff4b116c2e69b1b5816eb7ae1f
Benza4342 <duck5210@hotmail.com>
2016-12-08 14:02:23 +0700

    init

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 /* global firebase */
-var app = angular.module('app', []);
+var app = angular.module('app', [])
 app.controller('LoginCtrl', function ($window) {
     var config = {
         apiKey: "AIzaSyD-3iC1D0F8kLgb4OiVuMsZuH8M4daYXBE",
@@ -7,43 +7,43 @@ app.controller('LoginCtrl', function ($window) {
         databaseURL: "https://water-ec2fa.firebaseio.com",
         storageBucket: "water-ec2fa.appspot.com",
         messagingSenderId: "547238191292"
-    };
-    firebase.initializeApp(config);
-    var vm = this;
+    }
+    firebase.initializeApp(config)
+    var vm = this
     vm.fblogin = () => {
-        var provider = new firebase.auth.FacebookAuthProvider();
+        vm.fbloaded = 'loading'
+        var provider = new firebase.auth.FacebookAuthProvider()
         firebase.auth().signInWithPopup(provider).then(function (result) {
-            var token = result.credential.accessToken;
-            var user = result.user;
-            writeUserData(user);
+            writeUserData(user)
         }).catch(function (error) {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            var email = error.email;
-            var credential = error.credential;
-        });
+            vm.fbloaded = ''
+        })
 
-    };
+    }
     vm.login = () => {
-        const user = firebase.auth().currentUser;
-        var starCountRef = firebase.database().ref('users/' + user.uid);
-        starCountRef.on('value', function (snapshot) {
-            debugger;
-            //updateStarCount(postElement, snapshot.val());
-        });
-    };
+         vm.loginloaded = 'loading'
+        firebase.auth().signInWithEmailAndPassword(vm.email, vm.password).then(function (result) {
+            writeUserData(user)
+        }).catch(function (error) {
+            vm.loginloaded = ''
+        })
+    }
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
-            console.log('User is signed in.');
-            $window.location.href = 'room.html';
+            writeUserData(user)
+            console.log('User is signed in.')
         } else {
-            console.log('No user is signed in.');
+            console.log('No user is signed in.')
         }
-    });
+    })
     function writeUserData(user) {
         firebase.database().ref('users/' + user.uid).set({
-            displayName: user.displayName,
+            uid: user.uid,
             email: user.email
-        });
+        }).then(function () {
+            vm.loginloaded = ''
+            vm.fbloaded = ''
+            $window.location.href = 'room.html'
+        })
     }
-});
+})
--- public/room.js
+++ public/room.js
@@ -1,79 +1,107 @@
 var config = {
-    apiKey: "AIzaSyD-3iC1D0F8kLgb4OiVuMsZuH8M4daYXBE",
-    authDomain: "water-ec2fa.firebaseapp.com",
-    databaseURL: "https://water-ec2fa.firebaseio.com",
-    storageBucket: "water-ec2fa.appspot.com",
-    messagingSenderId: "547238191292"
+  apiKey: 'AIzaSyD-3iC1D0F8kLgb4OiVuMsZuH8M4daYXBE',
+  authDomain: 'water-ec2fa.firebaseapp.com',
+  databaseURL: 'https://water-ec2fa.firebaseio.com',
+  storageBucket: 'water-ec2fa.appspot.com',
+  messagingSenderId: '547238191292'
 }
 firebase.initializeApp(config)
 var database = firebase.database()
 var app = angular.module('app', [])
-$('#menu').click(function () {
-    $('.ui.sidebar').sidebar('show')
-})
-app.controller('RoomCtrl', function ($window, $scope) {
+app
+  .controller('RoomCtrl', function ($window, $scope) {
     vm = this
     vm.scope = $scope
-    vm.list = []
-    vm.showdetail = (id) => {
-        alert(id)
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) { vm.getdata() } else {
+        vm.loading = ''
+        window.location.href = 'index.html'
+      }
+    })
+    vm.logout = () => {
+      vm.loading = 'active'
+      firebase.auth().signOut()
     }
-    vm.addOwner = () => {
-        $('.ui.modal').modal('show')
-        //vm.writeOwner()
+    vm.toggle = () => { $('.ui.sidebar').sidebar('show') }
+    vm.modalOwner = () => {
+      $('#modalOwner').modal({
+        onHide: function () {
+          vm.onsave()
+          return true
+        }
+      }).modal('show')
+    }
+    vm.modalRoom = (ownerid, roomid) => {
+      vm.OwnerId = ownerid
+      vm.RoomId = roomid
+      if (roomid != 0) {
+        vm.RoomId = roomid
+        var roomref = firebase.database().ref('rooms/' + vm.OwnerId + '/' + roomid)
+        roomref.once('value', function (snapshot) {
+          vm.room = snapshot.val()
+          vm.scope.$apply()
+        })
+      }
+      $('#modalRoom').modal({
+        onHide: function () {
+          vm.onsave()
+          return true
+        }
+      }).modal('show')
     }
     vm.getdata = () => {
-        vm.list.splice(0, vm.list.length)
-        const user = firebase.auth().currentUser
-        var ownersref = firebase.database().ref('owners/' + user.uid)
-        ownersref.once('value', function (snapshot) {
+      vm.loading = 'active'
+      vm.list = []
+      var ownersref = firebase.database().ref('owners/' + firebase.auth().currentUser.uid)
+      ownersref.once('value', function (snapshot) {
+        snapshot.forEach(function (childSnapshot) {
+          var obOwner = childSnapshot.val()
+          obOwner.id = childSnapshot.key
+          obOwner.roomlist = []
+          var roomref = firebase.database().ref('rooms/' + childSnapshot.key)
+          roomref.once('value', function (snapshot) {
             snapshot.forEach(function (childSnapshot) {
-                var obOwner = childSnapshot.val()
-                obOwner.id = childSnapshot.key
-                obOwner.roomlist = []
-                var roomref = firebase.database().ref('rooms/' + childSnapshot.key)
-                roomref.on('value', function (snapshot) {
-                    snapshot.forEach(function (childSnapshot) {
-                        var childData = childSnapshot.val()
-                        childData.id = childSnapshot.key
-                        obOwner.roomlist.push(childData)
-                    })
-                    vm.list.push(obOwner)
-                    vm.scope.$apply()
-                })
+              var childData = childSnapshot.val()
+              childData.id = childSnapshot.key
+              obOwner.roomlist.push(childData)
             })
+            vm.list.push(obOwner)
+            vm.loading = ''
+            vm.scope.$apply()
+          })
         })
+      })
     }
-    vm.writeOwner = () => {
-        const user = firebase.auth().currentUser
-        var ownersref = firebase.database().ref('owners/' + user.uid)
-        var newOwnerRef = ownersref.push()
-        id = newOwnerRef.key
-        newOwnerRef.set({ name: 'test5' })
-        newOwnerRef.on('child_added', function (data) {
-            roomRef = firebase.database().ref('rooms/' + id)
-            // for (i = 0 i <= 3 i++) {
-            //     var newroomRef = roomRef.push()
-            //     newroomRef.set({ roomno: '12/88', update: '12/01/2016', meter: 2000 })
-            // }
-            vm.getdata()
-        })
-
+    vm.saveOwner = () => {
+      var ownersref = firebase.database().ref('owners/' + firebase.auth().currentUser.uid)
+      var newOwnerRef = ownersref.push()
+      id = newOwnerRef.key
+      vm.owner.create = new Date().getTime()
+      newOwnerRef.set(vm.owner)
+      vm.getdata()
     }
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            console.log('User is signed in.')
-            vm.getdata()
-            //window.location.href = 'roomOwner.html'
-        } else {
-            console.log('No user is signed in.')
-            // window.location.href = 'index.html'
-        }
-    })
-}).directive('myRepeatDirective', function () {
+    vm.saveRoom = () => {
+      var roomref = firebase.database().ref('rooms/' + vm.OwnerId)
+      if (vm.RoomId == 0) {
+        var newroomRef = roomref.push()
+        vm.room.create = new Date().getTime()
+        vm.room.modify = new Date().getTime()
+      } else {
+        var newroomRef = firebase.database().ref('rooms/' + vm.OwnerId + '/' + vm.RoomId)
+        vm.room.modify = new Date().getTime()
+      }
+      newroomRef.set(vm.room)
+      vm.getdata()
+    }
+    vm.onsave = () => {
+      vm.OwnerId = 0
+      vm.RoomId = 0
+      vm.room = {}
+      vm.owner = {}
+    }
+  })
+  .directive('myRepeatDirective', function () {
     return function (scope, element, attrs) {
-        $('.ui.accordion').accordion()
+      $('.ui.accordion').accordion()
     }
-})
-
-
+  })
